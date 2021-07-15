7d7b2c40f8d6f82350d12b77c1ef9516c6cfdb17
SeungHwan Jeon <43638373+MonkCodingg@users.noreply.github.com>
2021-07-15 09:02:21 +0900

    로그인 폼 컴포넌트 생성 및 마크업

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <form>
+      <div>
+        <label for="username">id:</label>
+        <input id="username" type="text" />
+      </div>
+      <div>
+        <label for="password">pw:</label>
+        <input id="password" type="text" />
+      </div>
+      <button type="submint">로그인</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
