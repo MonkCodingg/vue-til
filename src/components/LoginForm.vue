a79240e47b6ff3e1a7125f6b1297f0ef96390612
Seunghwan Jeon <laevus.j@gmail.com>
2021-08-04 11:56:37 +0900

    action 속성을 이용한 로그인 기능구현과 비동기 처리

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -29,9 +29,8 @@
 </template>
 
 <script>
-import { loginUser } from '@/api/index';
 import { validateEmail } from '@/utils/validation';
-import { saveAuthToCookie, saveUserToCookie } from '@/utils/cookies';
+
 export default {
   data() {
     return {
@@ -55,14 +54,10 @@ export default {
           username: this.username,
           password: this.password,
         };
-        const { data } = await loginUser(userData);
-        console.log(data.token);
-        this.$store.commit('setToken', data.token);
-        this.$store.commit('setUsername', data.user.username);
-        saveAuthToCookie(data.token);
-        saveUserToCookie(data.user.username);
 
-        this.$router.push('main');
+        await this.$store.dispatch('LOGIN', userData);
+
+        this.$router.push('/main');
       } catch (error) {
         //에러 핸들링할 코드
         console.log(error.response.data);
