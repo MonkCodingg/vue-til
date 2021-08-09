66aba5f50bd7f87f1774d4f64cb12559c59fabf8
Seunghwan Jeon <laevus.j@gmail.com>
2021-08-09 15:16:45 +0900

    API 함수 모듈화-인스턴스 생성함수 분할, 계정 관련 API 분할

--- src/api/index.js
+++ src/api/index.js
@@ -2,21 +2,23 @@ import axios from 'axios';
 import { setInterceptors } from '@/api/common/interceptors';
 
 function createInstance() {
-  const instance = axios.create({
+  return axios.create({
     baseURL: process.env.VUE_APP_API_URL,
+  });
+}
+
+// axios 초기화 함수
+function createInstanceWithAuth(url) {
+  const instance = axios.create({
+    baseURL: `${process.env.VUE_APP_API_URL}${url}`,
   });
 
   return setInterceptors(instance);
 }
-const instance = createInstance();
 
-function registerUser(userData) {
-  return instance.post('signup', userData);
-}
-// 로그인 API
-function loginUser(userData) {
-  return instance.post('login', userData);
-}
+export const instance = createInstance();
+const posts = createInstanceWithAuth('posts');
+
 // 학습 노트 데이터 조회 API
 function fetchPosts() {
   return instance.get('posts');
@@ -25,4 +27,7 @@ function fetchPosts() {
 function createPosts(postData) {
   return instance.post('posts', postData);
 }
+
+CREATE - post;
+
 export { registerUser, loginUser, fetchPosts, createPosts };
