3826847799127b23ea779acd8dfa987e172b8874
Seunghwan Jeon <laevus.j@gmail.com>
2021-08-11 15:13:53 +0900

    학습 노트 수정 페이지 생성 및 라우터 연결

--- src/routes/index.js
+++ src/routes/index.js
@@ -27,6 +27,10 @@ export default new VueRouter({
       component: () => import('@/views/PostAddPage.vue'),
     },
     {
+      path: '/post/:id',
+      component: () => import('@/views/PostEditePage.vue'),
+    },
+    {
       path: '*',
       component: () => import('@/views/NotFoundPage.vue'),
     },
