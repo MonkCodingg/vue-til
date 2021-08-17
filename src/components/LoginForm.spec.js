877ca85abf74d83ca8400d8388a72e072dffeeca
Seunghwan Jeon <laevus.j@gmail.com>
2021-08-17 10:58:20 +0900

    로그인 폼의 인풋 박스 관련 테스트 코드 작성

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -3,8 +3,14 @@ import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
   test('id는 이메일 형식이어야 함.', () => {
-    const wrapper = shallowMount(LoginForm);
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'test',
+        };
+      },
+    });
     const idInput = wrapper.find('#username');
-    console.log(idInput.html());
+    console.log(idInput.element.value);
   });
 });
