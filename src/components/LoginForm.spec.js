f1ed61a9053dabb888a4170538a21bc3d00010ce
Seunghwan Jeon <laevus.j@gmail.com>
2021-08-18 11:48:08 +0900

    로크인 컴포넌트 테스트코드 작성

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -2,16 +2,30 @@ import { shallowMount } from '@vue/test-utils';
 import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-  test('id는 이메일 형식이어야 함.', () => {
+  test('id가 이메일 형식이 아니면 경고 메시지가 출력됨.', () => {
     const wrapper = shallowMount(LoginForm, {
       data() {
         return {
-          username: 'test@abc.com',
+          username: 'test',
         };
       },
     });
-    const idInput = wrapper.find('#username');
-    console.log('인풋 박스의 값 ', idInput.element.value);
-    console.log(wrapper.vm.isUsernameValid);
+    const warningText = wrapper.find('.warning');
+    //console.log(warningText.html());
+    expect(warningText.exists()).toBeTruthy();
+  });
+
+  test('id와 pw가 입력되지 않으면 로그인 버튼이 비활성화 됨', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'a@a.com',
+          password: '1234',
+        };
+      },
+    });
+    const button = wrapper.find('button');
+    // button.element.disabled
+    expect(button.element.disabled).toBeTruthy();
   });
 });
